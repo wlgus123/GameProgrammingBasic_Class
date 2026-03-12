f0380b877b69f91308b284bb1df035589af97089
wlgus123 <mail30019794@gmail.com>
2026-03-12 12:44:25 +0900

    자료형

--- GameProgrammingBasic_Class/Program.cs
+++ GameProgrammingBasic_Class/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
 	static void Main(string[] args)
 	{
+		byte b;     // 기본값: unsigned
+		sbyte sb;   // signed를 명시
+		int i;
+		uint ui;
+
 		// 출력 (줄바꿈)
 		System.Console.Write("라인1");
 		Console.WriteLine("라인2");       // \n
