2d3fdfc96637ee5051332a2a04783eb546e35ae6
wlgus123 <mail30019794@gmail.com>
2026-03-12 12:46:57 +0900

    형변환

--- GameProgrammingBasic_Class/Program.cs
+++ GameProgrammingBasic_Class/Program.cs
@@ -10,6 +10,12 @@ class Program
 		uint ui;
 		object obj; // 모든 자료형 가져올 수 있음 (== void*) <- 근데 느림(많이 사용은 X)
 
+		string tempStr = "123";
+		int tempInt = Convert.ToInt32(tempStr);
+		float tempFloat = Convert.ToSingle(tempStr);    // float == single
+		string tempStr2 = "1ab23";
+		int tempInt2 = int.Parse(tempStr2);     // error
+
 		// 출력 (줄바꿈)
 		System.Console.Write("라인1");
 		Console.WriteLine("라인2");       // \n
