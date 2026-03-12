947cbe96605b77343f3d30d0c00f190bca72b9d5
wlgus123 <mail30019794@gmail.com>
2026-03-12 12:43:12 +0900

    출력

---
+++ GameProgrammingBasic_Class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// 출력 (줄바꿈)
+		System.Console.Write("라인1");
+		Console.WriteLine("라인2");       // \n
+		Console.WriteLine("라인3");
+	}
+}
